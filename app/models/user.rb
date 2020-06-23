d0d48f5aee0fd56798778094443fab1fb08b459d
n.isobe <ganmodoki7.43dari@gmail.com>
2020-06-23 11:26:45 +0900

    ユーザーのフォロー機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                    foreign_key: "follower_id",
+                    dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                    foreign_key: "followed_id",
+                    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token
   before_save {self.email = email.downcase}
   validates :name, presence: true, length: {maximum: 80}
@@ -36,6 +44,22 @@ class User < ApplicationRecord
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    # <<で配列の最後に追記できる
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしていたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
 end
